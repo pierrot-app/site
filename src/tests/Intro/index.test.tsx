9924ca46de1db77fce2d46b24f30abb6f655b225
Quentin Tshaimanga <quentin@macbook-air-de-marine.home>
2018-03-27 00:43:04 +0200

    Updates

--- src/tests/Intro/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Intro from './../../components/Intro/';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Intro />, div);
-});
