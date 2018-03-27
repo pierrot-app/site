2cbab790f58df8e1cbdafbd60bc3d51635448d37
Quentin Tshaimanga <quentin@macbook-air-de-marine.home>
2018-03-28 01:47:58 +0200

    update form

--- src/components/Elements/EmailInput/index.tsx
+++ src/components/Elements/EmailInput/index.tsx
@@ -1,32 +1,46 @@
 import * as React from 'react';
 import './email-input.scss';
 
-class EmailInput extends React.Component<{}, {}> {
+interface EmailInputState {
+  value: string;
+}
+
+class EmailInput extends React.Component<{}, EmailInputState> {
 
   constructor(props: {}) {
     super(props);
-  }
 
-  public sendEmail() {
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbxZC-0f0m5QgehyFWWcM6SPVV8BNyfDogEpu80gFrEPzQSOhHgI/exec';
-    const form = document.forms['submit-to-google-sheet'];
-    form.addEventListener('submit', (event: Event) => {
-      console.log(event, form);
-      event.preventDefault();
-      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => console.log('Success!', response))
-        .catch(error => console.error('Error!', error.message));
-    });
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
   }
+
+  public handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+
+    this.setState({value: event.target.value});
+
+  }
+
+  public handleSubmit(event: React.FormEvent<HTMLButtonElement>) {
+
+   event.preventDefault();
+   if (this.state.value !== '') {
+     const scriptURL = 'https://script.google.com/macros/s/AKfycbxZC-0f0m5QgehyFWWcM6SPVV8BNyfDogEpu80gFrEPzQSOhHgI/exec';
+     const formData = new FormData();
+     formData.append('email', this.state.value);
+     fetch(scriptURL, { method: 'POST', body: formData })
+       .then(response => response)
+       .catch(error =>  error.message);
+   }
+
+ }
 
   render() {
     return (
       <div className="email-input">
-        <form name="submit-to-google-sheet">
-          <input className="input-text" type="text" name="email" placeholder="Entrez votre addresse email"/>
-          <button className="input-submit" type="submit" onClick={this.sendEmail}>ça m'interesse</button>
-        </form>
+        <input className="input-text" type="text" value={this.state.value} onChange={e => this.handleChange(e)} placeholder="Entrez votre addresse email"/>
+        <button className="input-submit" type="submit" onClick={e => this.handleSubmit(e)}>ça m'interesse</button>
       </div>
     );
   }
