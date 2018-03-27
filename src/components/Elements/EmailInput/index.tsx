92de3dc3991b7666a4b198a070fc815f03398e7c
Quentin Tshaimanga <quentin@macbook-air-de-marine.home>
2018-03-28 00:27:12 +0200

    email indexation

--- src/components/Elements/EmailInput/index.tsx
+++ src/components/Elements/EmailInput/index.tsx
@@ -1,13 +1,31 @@
 import * as React from 'react';
 import './email-input.scss';
 
-class EmailInput extends React.Component {
+class EmailInput extends React.Component<{}, {}> {
+
+  constructor(props: {}) {
+    super(props);
+  }
+
+  public sendEmail() {
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxZC-0f0m5QgehyFWWcM6SPVV8BNyfDogEpu80gFrEPzQSOhHgI/exec';
+    const form = document.forms['submit-to-google-sheet'];
+    form.addEventListener('submit', (event: Event) => {
+      console.log(event, form);
+      event.preventDefault();
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message));
+    });
+
+  }
+
   render() {
     return (
       <div className="email-input">
-        <form action="">
-          <input className="input-text" type="text" placeholder="Entrez votre addresse email" />
-          <input className="input-submit" type="submit" value="ça m'interesse" />
+        <form name="submit-to-google-sheet">
+          <input className="input-text" type="text" name="email" placeholder="Entrez votre addresse email"/>
+          <button className="input-submit" type="submit" onClick={this.sendEmail}>ça m'interesse</button>
         </form>
       </div>
     );
