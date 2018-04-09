1ac6a0a34eb8c4a8903482e3dd7666831d1d7ef6
Leo Ternoir <leo@leoternoir.com>
2018-04-09 17:37:37 +0200

    fix minor text errors

--- src/components/EmailInput/index.tsx
+++ src/components/EmailInput/index.tsx
@@ -51,7 +51,7 @@ class EmailInput extends React.Component<EmailInputProps, EmailInputState> {
     return (
       <div className={`${this.props.className} email-input`}>
         <input className="input-text" type="text" value={this.state.value} onChange={e => this.handleChange(e)} placeholder="Entrez votre email"/>
-        <button className="input-submit" type="submit" onClick={e => this.handleSubmit(e)}>Ça m'int&#233;resse</button>
+        <button className="input-submit" type="submit" onClick={e => this.handleSubmit(e)}>&#199;a m'int&#233;resse</button>
       </div>
     );
   }
